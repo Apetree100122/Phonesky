6dd692376769855c5fad91fc0d72440cad9329c8
Alexander Petree <apetree1001@email.phoenix.edu>
2024-08-28 03:12:37 -0700

    Update TosAckedReceiver.java

--- src/org/microg/playstore/TosAckedReceiver.java
+++ src/org/microg/playstore/TosAckedReceiver.java
@@ -1,11 +1,10 @@
-package org.microg.playstore;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-
-public class TosAckedReceiver extends BroadcastReceiver {
-	@Override
-	public void onReceive(Context context, Intent intent) {
-	}
+package org.microg.playstore; import android.content.BroadcastReceiver; import 
+	android.content.Context; import android.content.Intent;
+public class TosAckedReceiver extends
+	BroadcastReceiver { @Override
+	public 
+			   void onReceive
+			   (Context context, 
+			    Intent intent)
+			   {}
 }
